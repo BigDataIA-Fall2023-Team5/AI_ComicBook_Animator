f8a45ecd08990e5c0e6c2dc6114cb82b1286d8f4
mundargi.s@northeastern.edu mundargishruti Shruti Suresh Mundargi <mundargi.s@northeastern.edu>
2023-12-15 11:38:06 -0500

    Pipeline 3

--- airfloww-Pipeline3/dags/sandbox.py
+++ airfloww-Pipeline3/dags/sandbox.py
@@ -1,20 +1,24 @@
-import os
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.models.param import Param
 from datetime import timedelta
-import os
 import openai
 from openai import OpenAI
 import requests
 import base64
 import re
 from pymongo import MongoClient
+from google.cloud import storage
+import os
+from google.cloud import firestore
+import boto3
+
+
 
 user_input = {
-    "datasource_url1": Param(default="https://www.sec.gov/files/form1.pdf", type='string', minLength=5, maxLength=255),
+    "play_name": Param(default="MACBETH", type='string', minLength=5, maxLength=255),
 }
 
 dag = DAG(
@@ -27,8 +31,25 @@ dag = DAG(
     params=user_input,
 )
 
+def retrieve_from_firestore(**kwargs):
+    db = firestore.Client()
+    collection_ref = db.collection('plays')  
+    documents = collection_ref.stream()
+
+    chapter_name = kwargs["params"]["play_name"]
+    parts = []
+    for doc in documents:
+        if chapter_name in doc.id:
+            parts.append(doc.to_dict().get('segments'))
+            print(f'Matching {doc.id} => {doc.to_dict()}')
+        else:
+            print(f'Non-matching {doc.id}')
+    return parts
+
 def prompt_engineering(**kwargs):
-    openai.api_key = os.getenv("OPENAI_KEY")
+    os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+    parts = retrieve_from_firestore(**kwargs)
 
     comic_prompts = []
 
@@ -41,9 +62,7 @@ def prompt_engineering(**kwargs):
             messages=[
             {
             "role": "system",
-            "content": """this text is a story, how to chunk this text like this when you want to use those chunks to form prompts for image generation to form a comic strip out of the whole text. Give me image prompts in such a way that I can create a sensible comic strip out of those image prompts. Give 8 chunks in format: \n 1. Image prompt: "Text example"
-    2. Image prompt: "Text example"
-    3. Image prompt: "Text example"\n"""
+            "content": """this text is a story, how to chunk this text like this when you want to use those chunks to form prompts for image generation to form a comic strip out of the whole text. Give me image prompts in such a way that I can create a sensible comic strip out of those image prompts. Keep it kid-friendly. Give 5 chunks exactly in format: \n "1. Image prompt: Text example 2. Image prompt: Text example 3. Image prompt: Text example..."\n"""
             },
             {
             "role": "user",
@@ -92,7 +111,7 @@ def api_call(prompt, image_number):
             "weight": 1
         },
         {
-            "text": "blurry, bad",
+            "text": "blurred, bad, disfigured, black and white",
             "weight": -1
         }
         ],
@@ -114,41 +133,51 @@ def api_call(prompt, image_number):
         raise Exception("Non-200 response: " + str(response.text))
 
     data = response.json()
-
+    
     # make sure the out directory exists
-    if not os.path.exists("./out"):
-        os.makedirs("./out")
+    if not os.path.exists("/opt/airflow/dags/out"):
+        os.makedirs("/opt/airflow/dags/out")
 
     for i, image in enumerate(data["artifacts"]):
-        with open(f'./out/image_{image_number}_{i}.png', "wb") as f:
+        with open(f'/opt/airflow/dags/out/image_{image_number}_{i}.png', "wb") as f:
             f.write(base64.b64decode(image["base64"]))
 
 
 def stable_diffusion(**kwargs):
     separated_prompts = prompt_engineering(**kwargs)
     image_number = 1
-
+    
     for prompt in separated_prompts:
         print(image_number)
-        api_call(prompt, image_number)
+        try:
+            api_call(prompt, image_number)    
+        except Exception as e:
+            print(f"An error occurred: {e}")
         image_number += 1
 
+
 def store_images_and_prompts(**kwargs):
-    image_folder_path = './out'
+    image_folder_path = "/opt/airflow/dags/out"
     separated_prompts = prompt_engineering(**kwargs)
-    client = MongoClient('mongodb_connection_string')  # Replace with your MongoDB connection string
-    db = client['your_database']  # Replace with your database name
-    collection = db['your_collection']  # Replace with your collection name
+
+    # Set up MongoDB connection
+    client = MongoClient('')
+    db = client['images_and_prompts']
+    collection = db['images_prompts']
+
+    # Set up S3 client
+    s3_client = boto3.client('s3')
+    bucket_name = ''
 
     def is_valid_image(filename):
         parts = filename.split('_')
         if len(parts) == 3 and parts[0] == 'image' and parts[2].endswith('.png'):
             try:
-                int(parts[1])  # Check if the middle part is an integer
+                int(parts[1])
                 return True
             except ValueError:
-                return False
-        return False
+                return True
+        return True
 
     image_files = sorted(
         filter(is_valid_image, os.listdir(image_folder_path)),
@@ -156,19 +185,24 @@ def store_images_and_prompts(**kwargs):
     )
 
     num_iterations = min(len(image_files), len(separated_prompts))
-
+   
     for i in range(num_iterations):
         image_file = image_files[i]
         text = separated_prompts[i]
         image_path = os.path.join(image_folder_path, image_file)
 
-        with open(image_path, 'rb') as file:
-            encoded_image = file.read()
+       
+        s3_client.upload_file(image_path, bucket_name, image_file) 
 
+
+        image_url = f'https://{bucket_name}.s3.amazonaws.com/{image_file}'
+
+        # Store the reference in MongoDB
         document = {
             'image_name': image_file,
             'prompt': text,
-            'image_data': encoded_image  # Storing the binary data of the image
+            'image_url': image_url,
+            'story_name': kwargs["params"]["play_name"] # Storing the URL of the image
         }
         collection.insert_one(document)
 
@@ -177,6 +211,12 @@ def store_images_and_prompts(**kwargs):
 
 
 with dag:
+    retrieve_from_firestore_task = PythonOperator(
+        task_id="retrieve_from_firestore",
+        python_callable=retrieve_from_firestore,
+        dag=dag,
+    )
+
     prompt_engineering_task = PythonOperator(
         task_id="prompt_engineering",
         python_callable=prompt_engineering,
@@ -195,9 +235,17 @@ with dag:
         dag=dag,
     )
 
+    
+
     bye_world_task = BashOperator(
         task_id="bye_world",
         bash_command='echo "Bye from airflow"'
     )
 
-    prompt_engineering_task >> stable_diffusion_task >> store_images_and_prompts_task >> bye_world_task
+    retrieve_from_firestore_task >> prompt_engineering_task >> stable_diffusion_task >> store_images_and_prompts_task >> bye_world_task
+
+
+
+
+
+
